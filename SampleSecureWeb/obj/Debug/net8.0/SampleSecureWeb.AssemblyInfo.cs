a985f4308c6fb651b94527f377aad1638acb08c6
Albertt <chrisalberttt5@gmail.com>
2024-10-09 22:13:02 +0700

    Implementasi Registrasi dan Login

--- SampleSecureWeb/obj/Debug/net8.0/SampleSecureWeb.AssemblyInfo.cs
+++ SampleSecureWeb/obj/Debug/net8.0/SampleSecureWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleSecureWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e8281f477c58102214c1a838562e4f6d68e80b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleSecureWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleSecureWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
